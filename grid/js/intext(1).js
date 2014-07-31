755719f5865db1ba76faa5ff1ffcab13f4573580
Pushti <pushti.shah92@gmail.com>
2014-07-31 13:46:05 +0530

    add files

---
+++ grid/js/intext(1).js
@@ -0,0 +1,9 @@
+ var $adtextsup = (function (window) 
+  { 
+   var appNamespace = '$adtextsup'; 
+   var pub={};
+   var version = ''; 
+   var opts = {  s1: '', s3: '', cookies: 'branduid=ed75ac9e4e1f769af1bd00d2fea63181|1405404620; vc=2'};
+   pub.config= function () { return('<sbinfo>?</sbinfo><now>2014-07-15 02:35:43</now>');}
+   return(pub);
+ }(window)); try { $adtext.analyzePage();} catch(e){}
